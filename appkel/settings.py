f93c5d62463254bfbc56e71940081d89cf468447
mugen_champloo <abdurahimmahau@gmail.com>
2023-09-18 19:50:03 +0600

    postgesql_v2

--- appkel/settings.py
+++ appkel/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'PORT': '5432',       # Порт PostgreSQL сервера (по умолчанию 5432)
     }
 }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
