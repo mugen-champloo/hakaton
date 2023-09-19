c440cba6b29019a1481d574f61934743ed8ccd18
mugen_champloo <abdurahimmahau@gmail.com>
2023-09-19 19:18:20 +0600

    Some fix in models

--- api/models.py
+++ api/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
     image = models.CharField(max_length=300, null=True, verbose_name='ссылка на картинку')
     unit = models.CharField(max_length=100, verbose_name='Единица измерения')
     quantity = models.IntegerField(verbose_name='Количество за ед.измерения')
-    price = models.IntegerField(verbose_name='Цена')
+    price = models.FloatField(verbose_name='Цена')
     in_stock = models.BooleanField(default=False, verbose_name='В наличии')
 
     def __str__(self):
@@ -42,7 +42,8 @@ class Recipe(models.Model):
     energy = models.IntegerField(verbose_name='Энергетическая ценность')
     portion = models.CharField(max_length=50, verbose_name='На сколько человек')
     how_to_cook = models.TextField(max_length=300, verbose_name="Способ приготовления")
-    ingredients_for_cook = models.JSONField(blank=True, verbose_name='Ингредиенты для готовки')
+    ingredients_for_cook = models.CharField(blank=True, verbose_name='Ингредиенты для готовки')
+    ingredients_for_cook_quantity = models.CharField(blank=True, verbose_name='Ингредиенты для готовки')
 
     def __str__(self):
         return self.name
